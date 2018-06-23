65677581bdca74d2db52f8f73075a79496be74ae
debatanu-thakur <debatanu@gmail.com>
2018-06-24 01:15:40 +0200

    Add components for file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comp/header/header.component';
+import { FileUploaderComponent } from './comp/file-uploader/file-uploader.component';
+import { ProgressBarComponent } from './comp/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FileUploaderComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule
